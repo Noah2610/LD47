bcf231b0c4ad885ef56076c7939a9e7558b6cf8c
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-10-03 19:08:18 +0200

    Flip sprite in UpdateAnimationsSystem
    Closes #10

--- src/systems/update_animations.rs
+++ src/systems/update_animations.rs
@@ -9,14 +9,23 @@ impl<'a> System<'a> for UpdateAnimationsSystem {
     type SystemData = (
         WriteStorage<'a, AnimationsContainer<AnimationKey>>,
         ReadStorage<'a, Velocity>,
+        WriteStorage<'a, Transform>,
     );
 
     fn run(
         &mut self,
-        (mut animations_store, velocity_store): Self::SystemData,
+        (
+            mut animations_store,
+            velocity_store,
+            mut transform_store,
+        ): Self::SystemData,
     ) {
-        for (animations, velocity_opt) in
-            (&mut animations_store, velocity_store.maybe()).join()
+        for (animations, velocity_opt, transform) in (
+            &mut animations_store,
+            velocity_store.maybe(),
+            &mut transform_store,
+        )
+            .join()
         {
             if animations
                 .current()
@@ -27,6 +36,15 @@ impl<'a> System<'a> for UpdateAnimationsSystem {
 
                 // WALK
                 if let Some(velocity) = velocity_opt {
+                    // FLIP SPRITE
+                    if velocity.x > WALK_ANIM_PADDING {
+                        let scale = transform.scale_mut();
+                        scale.x = scale.x.abs() * 1.0;
+                    } else if velocity.x < -WALK_ANIM_PADDING {
+                        let scale = transform.scale_mut();
+                        scale.x = scale.x.abs() * -1.0;
+                    }
+
                     if (velocity.x > WALK_ANIM_PADDING
                         || velocity.x < -WALK_ANIM_PADDING)
                         && animations.has_animation(&AnimationKey::Walk)
