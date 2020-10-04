6937574ee44de8817a03f5f1ee6cb4aa9fcdef72
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-10-04 20:57:37 +0200

    Setup scene_1

--- tiled/tsx/scene1bg.tsx
+++ tiled/tsx/scene1bg.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="scene1bg" tilewidth="16" tileheight="16" tilecount="144" columns="16">
- <image source="../../resources/spritesheets/bg/scene1.png" width="256" height="144"/>
+ <image source="../../resources/spritesheets/bg/scene1_new.png" width="256" height="144"/>
 </tileset>
