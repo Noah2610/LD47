d69a8c087aa571c70486a880b9afe235510d6026
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-10-05 19:32:35 +0200

    Scene 1 config update

---
+++ tiled/tsx/scene1bg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="scene1bg" tilewidth="16" tileheight="16" tilecount="144" columns="16">
+ <image source="../../docs/concept art/scene1/bg.png" width="256" height="144"/>
+</tileset>
