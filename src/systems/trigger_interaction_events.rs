da1a73e42bf39b41d25dc9bfdd789f4e06eb3dd2
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-10-07 08:07:20 +0200

    Can't interact if player is not in control, update bed config

--- src/systems/trigger_interaction_events.rs
+++ src/systems/trigger_interaction_events.rs
@@ -29,34 +29,34 @@ impl<'a> System<'a> for TriggerInteractionEventsSystem {
         for (player, player_collider) in
             (&mut player_store, &collider_store).join()
         {
-            // if player.in_control {
-            let query_exp = {
-                use deathframe::physics::query::exp::prelude_variants::*;
-                IsTag(CollisionTag::Interactable)
-            };
+            if player.in_control {
+                let query_exp = {
+                    use deathframe::physics::query::exp::prelude_variants::*;
+                    IsTag(CollisionTag::Interactable)
+                };
 
-            if input_manager.is_down(IngameAction::Interact) {
-                if let Some(colliding_with) = player_collider
-                    .query::<FindQuery<_>>()
-                    .exp(&query_exp)
-                    .run()
-                {
-                    for (entity, interactable, events_register) in (
-                        &entities,
-                        &interactable_store,
-                        &mut events_register_store,
-                    )
-                        .join()
+                if input_manager.is_down(IngameAction::Interact) {
+                    if let Some(colliding_with) = player_collider
+                        .query::<FindQuery<_>>()
+                        .exp(&query_exp)
+                        .run()
                     {
-                        if entity.id() == colliding_with.id {
-                            events_register
-                                .trigger_event(&EventType::OnInteract);
-                            break;
+                        for (entity, interactable, events_register) in (
+                            &entities,
+                            &interactable_store,
+                            &mut events_register_store,
+                        )
+                            .join()
+                        {
+                            if entity.id() == colliding_with.id {
+                                events_register
+                                    .trigger_event(&EventType::OnInteract);
+                                break;
+                            }
                         }
                     }
                 }
             }
-            // }
         }
     }
 }
